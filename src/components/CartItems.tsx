bdcc39f35979a4cb8c2d9b8bcb6a3ef4d711986b
Bre Gartland <bthunholm@gmail.com>
2026-02-03 12:21:23 -0600

    Format total price and get cart items from store for CartItems component

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,33 +1,40 @@
+import { useCartSelector } from "../store/hooks";
+
 export default function CartItems() {
+  const cartItems = useCartSelector((state) => state.cart.items);
+  const totalPrice = cartItems.reduce(
+    (val, item) => val + item.price * item.quantity,
+    0,
+  );
+  const formattedTotalPrice = totalPrice.toFixed(2);
+
   return (
     <div id="cart">
       <p>No items in cart!</p>
 
-      {/* <ul id="cart-items">
-          {cartItems.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+      <ul id="cart-items">
+        {cartItems.map((item) => {
+          const formattedPrice = `$${item.price.toFixed(2)}`;
 
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.title}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => handleRemoveFromCart(item.id)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => handleAddToCart(item)}>+</button>
-                </div>
-              </li>
-            );
-          })}
-        </ul> */}
+          return (
+            <li key={item.id}>
+              <div>
+                <span>{item.title}</span>
+                <span> ({formattedPrice})</span>
+              </div>
+              <div className="cart-item-actions">
+                <button onClick={() => handleRemoveFromCart(item.id)}>-</button>
+                <span>{item.quantity}</span>
+                <button onClick={() => handleAddToCart(item)}>+</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
 
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+      <p id="cart-total-price">
+        Cart Total: <strong>${formattedTotalPrice}</strong>
+      </p>
     </div>
   );
 }
